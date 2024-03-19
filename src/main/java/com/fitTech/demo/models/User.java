fac16e204f41f65574855b711d8c8aa5cff42438
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2024-03-18 21:50:59 -0500

    UserModel

--- src/main/java/com/fitTech/demo/models/User.java
+++ src/main/java/com/fitTech/demo/models/User.java
@@ -1,6 +1,32 @@
 package com.fitTech.demo.models;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Entity
 public class User {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
 }
+
