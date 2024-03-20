2b35a862abb9d53aaac8744674e0bc7e9455e840
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2024-03-19 21:08:25 -0500

    addingDTO

---
+++ src/main/java/com/fitTech/demo/models/DTO/RegisterFormDTO.java
@@ -0,0 +1,15 @@
+package com.fitTech.demo.models.DTO;
+
+public class RegisterFormDTO extends LoginFormDTO{
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+}
