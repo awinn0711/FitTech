414da3a756c4bf74464f482e0253cb6d670088c8
LeedsByExample <leeda.minkin@yahoo.com>
2024-03-20 17:54:00 -0500

    started writing ingredient entity

--- src/main/java/com/fitTech/demo/models/Ingredient.java
+++ src/main/java/com/fitTech/demo/models/Ingredient.java
@@ -1,5 +1,20 @@
 package com.fitTech.demo.models;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Ingredient {
-    //insert code here
+    private String name;
+
+    public Ingredient(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
