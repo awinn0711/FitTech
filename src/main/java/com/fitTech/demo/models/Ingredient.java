8ff2b21eb00141f310a8ac87e8557da966271d6b
LeedsByExample <leeda.minkin@yahoo.com>
2024-03-21 18:57:31 -0500

    added generatedvalue strategy for ingredient entity

--- src/main/java/com/fitTech/demo/models/Ingredient.java
+++ src/main/java/com/fitTech/demo/models/Ingredient.java
@@ -2,13 +2,14 @@ package com.fitTech.demo.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Ingredient {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
@@ -42,4 +43,5 @@ public class Ingredient {
     public int getId() {
         return id;
     }
+
 }
