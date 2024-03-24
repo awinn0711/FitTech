c074e9a9cf9e8f9a512744a78d3f48dd9e902228
LeedsByExample <leeda.minkin@yahoo.com>
2024-03-24 15:30:54 -0500

    Created base recipe entity and recipe repository

---
+++ src/main/java/com/fitTech/demo/models/Recipe.java
@@ -0,0 +1,47 @@
+package com.fitTech.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int recipeId;
+
+    private String name;
+    private String description;
+
+    public Recipe() {
+
+    }
+
+    public Recipe(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+}
