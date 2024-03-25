43ec3941abb7aab6cccaf62492e753af243d3109
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2024-03-24 19:58:47 -0500

    addingAuth0

--- app/src/index.js
+++ app/src/index.js
@@ -4,11 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+<React.StrictMode>
+<Auth0Provider
+    domain="dev-htw138ror3btjs0l.us.auth0.com"
+    clientId="HFwIXB7zNHWyqH3mGeeOYnme35GyQaUf"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>
+
+
   </React.StrictMode>
 );
 
