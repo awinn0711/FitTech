a16caca08b77d42caf4ce2a9560d4eb42cfc7d0b
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2024-03-27 17:07:51 -0500

    addingAuth0Backend

---
+++ src/main/java/com/fitTech/demo/HomeServlet.java
@@ -0,0 +1,27 @@
+package com.fitTech.demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HomeServlet {
+    @WebServlet(urlPatterns = {"/portal/home"})
+    public class HomeServlet extends HttpServlet {
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+            final String accessToken = (String) SessionUtils.get(req, "accessToken");
+            final String idToken = (String) SessionUtils.get(req, "idToken");
+            if (accessToken != null) {
+                req.setAttribute("userId", accessToken);
+            } else if (idToken != null) {
+                req.setAttribute("userId", idToken);
+            }
+            req.getRequestDispatcher("/WEB-INF/jsp/home.jsp").forward(req, res);
+        }
+    }
+}
