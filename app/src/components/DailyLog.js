9a64490b31e8bec2785bc377660995aa95b63d99
Austin Winn <awinn0711@gmail.com>
2024-03-26 21:37:49 -0500

    fixed rendering issue

--- app/src/components/DailyLog.js
+++ app/src/components/DailyLog.js
@@ -3,28 +3,55 @@ import React, { useEffect, useState } from 'react';
 export default function DailyLog() {
 
     const [dailyLog, setDailyLog] = useState ({});
+    const [loading, setLoading] = useState(false);
+    const [date, setDate] = useState(null);
 
 //should fetch data from DailyLogController
-    useEffect(() => {
-        fetch('http://localhost:8080/api/dailylog')
-          .then(response => response.json())
-          .then(data => setDailyLog(data))
-          .catch(error => console.error('Error fetching data:', error));
+
+    // useEffect(() => {
+    //     setLoading(true);
+
+    //     fetch('http://localhost:8080/api/dailylog')
+    //       .then(response => response.json())
+    //       .then(data => {setDailyLog(data);
+    //                      setLoading(false);
+    //                      console.log(dailyLog);
+    //       })
+    //       .catch(error => console.error('Error fetching data:', error));
+
+    //       let todaysDate = dailyLog.date.date;
+    //       setDate(todaysDate);
+    //   }, []);
+      
+      async function fetchData() {
+        let response = await fetch("http://localhost:8080/api/dailylog");
+        let data = await response.json()
+        .then( data => {
+            setDailyLog(data)
+            let todaysDate = data.date.date
+            setDate(todaysDate)
+            setLoading(false)
+            });
+        };
+
+      useEffect(() => {
+        setLoading(true);
+        fetchData();
+        console.log(dailyLog);
+        console.log(date);
       }, []);
-      console.log(dailyLog);
-      let todaysDate;
-      todaysDate = dailyLog.date;
-      console.log(todaysDate);
-      let dateAsString;
-      dateAsString = String(todaysDate.date);
+      
 
 //As of now just attempting to return the current date as a header
 
-        const { data } = this.state;
-        if (!data) {
-            return <div>Loading...</div>;
+        if(loading) {
+            return <p>loading...</p>
         }
         return (
-            <h1>dateAsString</h1>
+            <div className='dailyLog'>
+                <h1 id ='date'>{date}</h1>
+                <div id='calories'>Today's Calories: </div>
+
+            </div>
         )
 };
