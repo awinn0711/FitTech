c1699190a073e0b17386b6201905b7cc08596a1e
LeedsByExample <leeda.minkin@yahoo.com>
2024-04-02 19:05:22 -0500

    Created basic add recipe page to add name, description, and ingredients

--- app/src/AddRecipe.js
+++ app/src/AddRecipe.js
@@ -3,35 +3,80 @@ import React, { useEffect, useState } from 'react';
 //just testing to see if I can create a webpage
 export default function AddRecipe() {
 
-    const [name, setName] = useState ("It's a me, Namio!");
-    const [loading, setLoading] = useState(true);
+    const [name, setName] = useState ("");
+    const [description, setDescription] = useState("");
+    const [ingredientsList, setIngredientsList] = useState([]);
+    const [ingredientInput, setIngredientInput] = useState("");
+    //const [loading, setLoading] = useState(true);
 
-    async function fetchData() {
-        let response = await fetch("http://localhost:8080/api/recipes");
-        let data = await response.json().then( data => {
-
-            // Setting React State From Retrieved Data
-            setLoading(false);
-            setName(data[0].name);
-        });
+    const handleIngredientChange = (e) => {
+        setIngredientInput(e.target.value);
     };
 
-    // Everything here is executed on page load
-    useEffect(() => {
-        fetchData();
-        // After data is retrieved, we can read it from the React State here
-    }, []);
+    //handler checks if ingredientInput is empty, and if it isn't, adds the current value of ingredientInput to the ingredientsList array
+    const handleAddIngredient = () => {
+        if (ingredientInput !== "") {
+            setIngredientsList([...ingredientsList, ingredientInput]);
+            setIngredientInput("");
+        }
+    };
 
-    if (loading) {
-        return (
-            <p>loading...</p>
-        )
-    } else {
-        return (
-            <p>{name}</p>
-        )
-    }
+
+    return (
+        <div>
+            <h2>Add Recipe</h2>
+            <form>
+                <label>
+                    Recipe Name:
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    Description:
+                    <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    Ingredients:
+                    <ul>
+                        {ingredientsList.map((ingredient, index) => (
+                            <li key={index}>{ingredient}</li>
+                        ))}
+                    </ul>
+                    <input type="text" value={ingredientInput} onChange={handleIngredientChange} />
+                    <button type="button" onClick={handleAddIngredient}>Add Ingredient</button>
+                </label>
+                <br />
+            </form>
+        </div>
+    );
+
 }
+//    async function fetchData() {
+//        let response = await fetch("http://localhost:8080/api/recipes");
+//        let data = await response.json().then( data => {
+//
+//            // Setting React State From Retrieved Data
+//            setLoading(false);
+//            setName(data[0].name);
+//        });
+//    };
+//
+//    // Everything here is executed on page load
+//    useEffect(() => {
+//        fetchData();
+//        // After data is retrieved, we can read it from the React State here
+//    }, []);
+//
+//    if (loading) {
+//        return (
+//            <p>loading...</p>
+//        )
+//    } else {
+//        return (
+//            <p>{name}</p>
+//        )
+//    }
 
 // functional code I will start to integrate later
 
@@ -66,3 +111,5 @@ export default function AddRecipe() {
 //      </form>
 //    </div>
 //  );
+
+//////////////////////IT IS WORKING AT THIS POINT
