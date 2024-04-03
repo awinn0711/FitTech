e7ae63b423ed9b79ca93413851cedd34617f8db2
LeedsByExample <leeda.minkin@yahoo.com>
2024-04-03 18:50:33 -0500

    removed unnecessary import statement

--- src/main/java/com/fitTech/demo/controllers/UserController.java
+++ src/main/java/com/fitTech/demo/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.fitTech.demo.controllers;
 
 import com.fitTech.demo.data.UserRepository;
 import com.fitTech.demo.models.User;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
