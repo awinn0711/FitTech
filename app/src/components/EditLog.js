cd6139da63300addc507345b67cbc07a6571e3d9
Austin Winn <awinn0711@gmail.com>
2024-04-06 16:51:59 -0500

    form displays, able to choose from saved recipes

--- app/src/components/EditLog.js
+++ app/src/components/EditLog.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { json } from 'react-router-dom';
 
 
 export default function EditLog() {
 
     const [recipes, setRecipes] = useState([]);
-    const [recipesToAdd, setRecipesToAdd] = useState([]);
+    const [recipeToAdd, setRecipeToAdd] = useState({});
     const [loading, setLoading] = useState(false);
 
     async function loadRecipes() {
@@ -16,15 +17,36 @@ export default function EditLog() {
         });
     };
 
-    const handleAddRecipeToList = (selection) => {
-         setRecipesToAdd([...recipesToAdd, selection])
+    const handleSelectRecipe = (event) => {
+        console.log("selected recipe", event.target.value);
+        setRecipeToAdd(JSON.parse(event.target.value));
     }
+
+    const handleAddRecipeToLog = () => {
+        fetch('http://localhost:8080/api/dailylog/edit', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(recipeToAdd),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to add meal');
+            }
+            console.log('Meal added successfully');
+        })
+        .catch(error => {
+            console.error('Error adding meal to log:', error.message);
+        });
+    };
+    
 
     useEffect(() => {
         setLoading(true);
         loadRecipes();
-        console.log("recipesToAdd: ", recipesToAdd);
-    }, []);
+        console.log("recipeToAdd: ", recipeToAdd);
+    }, [recipeToAdd]);
 
     if(loading) {
         return <p>loading...</p>
@@ -33,16 +55,17 @@ export default function EditLog() {
     return (
         <div>
             <h2>Add Meal to Today's Log</h2>
-            <ul>{recipesToAdd.map((selection) => (
-                <li key={selection.id}>{selection.name}</li>
-            ))}</ul>
-            <label for="recipes">Choose a recipe:</label>
-                <select name="recipes" onChange={handleAddRecipeToList}>{
-                    recipes.map((recipe) => (
-                        <option key={recipe.id} value={recipe.id}>{recipe.name}</option>
+            <form>
+            <label htmlFor="recipes">Choose a recipe:</label>
+                <select id="recipes" onChange={handleSelectRecipe}>
+                    <option value={null}>Saved Recipes</option>
+                    {recipes.map((recipe) => (
+                        <option key={recipe.id} value={JSON.stringify(recipe)}>{recipe.name}</option>
                     ))
                 }
                 </select>
+                <button type='button' onClick={handleAddRecipeToLog}>Add {recipeToAdd.name} to today's log</button>
+            </form>
         </div>
     );
-}
+};
