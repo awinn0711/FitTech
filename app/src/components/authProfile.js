10a383562870bf0fc70feaf99f8427018059cfda
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2024-04-14 16:57:50 -0500

    Adding Nav Bar

--- app/src/components/authProfile.js
+++ app/src/components/authProfile.js
@@ -1,11 +1,12 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
+
 const AuthProfile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return <div>Loading ...Please Wait</div>;
   }
 
   return (
@@ -14,6 +15,7 @@ const AuthProfile = () => {
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
+
       </div>
     )
   );
