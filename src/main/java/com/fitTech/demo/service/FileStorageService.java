f9933d9f24b34e1e046ba26ed05a4b26af7e0df1
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2024-04-21 12:53:51 -0500

    StoreImages

--- src/main/java/com/fitTech/demo/service/FileStorageService.java
+++ src/main/java/com/fitTech/demo/service/FileStorageService.java
@@ -10,23 +10,15 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.stream.Stream;
 
-@Service
+
 public interface FileStorageService {
 
-    @Autowired
-    FileDataRepository fileDataRepository = null;
-    public default FileData store(MultipartFile file) throws IOException {
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-        FileData FileData = new FileData(fileName, file.getContentType(), file.getBytes());
 
-        return fileDataRepository.save(FileData);
-    }
+    FileData store(MultipartFile file)throws IOException;
 
-    public default FileData getFile(String id) {
-        return fileDataRepository.findById(id).get();
-    }
 
-    public default Stream<FileData> getAllFiles() {
-        return fileDataRepository.findAll().stream();
-    }
+    FileData getFile(String id);
+
+    Stream<FileData> getAllFiles();
+
 }
